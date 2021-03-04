1cec566eaca4bcddb682d8deaa2224a37ce4cf96
janranz <djwadholm@gmail.com>
2021-03-04 01:39:22 -0800

    add 3.3/3.4

---
+++ 3.4/Lab - Scope.playground/Pages/1. Exercise - Scope.xcplaygroundpage/Contents.swift
@@ -0,0 +1,66 @@
+/*:
+ ## Exercise - Scope
+ 
+ Using a comment or print statement, describe why the code below will generate a compiler error if you uncomment line 10.
+*/
+for _ in 0..<10 {
+    let foo = 55
+    print("The value of foo is \(foo)")
+}
+//print("The value of foo is \(foo)")
+
+/*
+    Compiler error occurs on line 10 because the 'foo' that is being referenced only exists within the scope of the for loop
+ */
+/*:
+ Using a comment or print statement, describe why both print statements below compile when similar-looking code did not compile above. In what scope is `x` defined, and in what scope is it modified? In contrast, in what scope is `foo` defined and used?
+ */
+var x = 10
+for _ in 0..<10 {
+    x += 1
+    print("The value of x is \(x)")
+}
+print("The final value of x is \(x)")
+
+/*
+    In this case, variable 'x' exists within a scope that both the for loop and the outer print function resides. Global
+ */
+/*:
+ In the body of the function `greeting` below, use variable shadowing when unwrapping `greeting`. If `greeting` is successfully unwrapped, print a statement that uses the given greeting to greet the given name (i.e. if `greeting` successfully unwraps to have the value "Hi there" and `name` is `Sara`, print "Hi there, Sara."). Otherwise, use "Hello" to print a statement greeting the given name. Call the function twice, once passing in a value for greeting, and once passing in `nil`.
+ */
+func greeting(greeting: String?, name: String)
+{
+    if let greeting = greeting
+    {
+        print("\(greeting), \(name)?!")
+    } else
+    {
+        print("Get lost, \(name).")
+    }
+}
+greeting(greeting: "YO WHAT IS UP", name: "Karen")
+greeting(greeting: nil, name: "Andy")
+/*:
+ Create a class called `Car`. It should have properties for `make`, `model`, and `year` that are of type `String`, `String`, and `Int`, respectively. Since this is a class, you'll need to write your own memberwise initializer. Use shadowing when naming parameters in your initializer.
+ */
+class Car
+{
+    let make : String
+    let model : String
+    let year : Int
+    
+    init?(make: String?, model: String?, year: Int?)
+    {
+        guard let make = make,
+              let model = model,
+              let year = year else
+        {
+            return nil
+        }
+        self.make = make
+        self.model = model
+        self.year = year
+    }
+}
+
+//: page 1 of 2  |  [Next: App Exercise - Step Competition](@next)
